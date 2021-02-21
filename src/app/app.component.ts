b88eb7e03d9037a6b4b9dfd2f8ce1c66c1b8d405
macbook <12345678>
2021-02-21 18:27:56 +0100

    game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'examgame';
+
+user: any;
+userList: any = [];
+editIndex: number = 0;
+isRegister = false;
+
+constructor() {
+  this.user = {};
+}
+
+public add() {
+
+  this.userList.push(this.user);
+  this.user = {};
+  console.log(this.userList);
+}
+
+public save() {
+  this.isRegister = true;
+  if (this.editIndex != null) {
+    this.userList[this.editIndex].name = this.user.name;
+    this.userList[this.editIndex].gender = this.user.gender;
+  }
+
+}
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,30 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
-
 })
 export class GameComponent implements OnInit {
-  
-  user: any;
-  userList: any = [];
-  editIndex: number = 0;
-  
-  constructor() {
-    this.user = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
   }
-  
-  public add() {
-    
-    this.userList.push(this.user);
-    this.user = {};
-    console.log(this.userList);
-  }
-  
-  public save() {
-    if (this.editIndex != null) {
-      this.userList[this.editIndex].name = this.user.name;
-      this.userList[this.editIndex].gender = this.user.gender;
-    }
-  
-  }
-  }
+
+}
